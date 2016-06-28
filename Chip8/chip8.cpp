f57c17e84f4f9e030c0a3b2315a6a2c93f04e7ce
gulshngill <gulshn.gill@gmail.com>
2016-06-28 20:17:46 +0800

    Updated opcode

--- Chip8/chip8.cpp
+++ Chip8/chip8.cpp
@@ -113,6 +113,7 @@ void chip8::Emulate() {
         case 0x00E0: //clears the screen
           for(int i = 0; i < 2048; ++i)
             gfx[i] = 0;
+          pc += 2;
           break;
         case 0x00EE: //Returns from a subroutine
           --sp;    
@@ -129,17 +130,55 @@ void chip8::Emulate() {
       ++sp; //increment stack pointer by 1
       pc = opcode & 0x0FFF;
       break;
-    case 0x3000:
+    case 0x3000: //3XNN - Skips the next instruction if VX equals NN
+      if(V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
+        pc += 4;
+      else
+        pc += 2;
       break;
-    case 0x4000:
+    case 0x4000: //4XNN - Skips the next instruction if VX doesn't equal NN
+      if(V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
+        pc += 4;
+      else
+        pc += 2;
       break;
-    case 0x5000:
+    case 0x5000: //5XY0 - Skips the next instruction if VX equals VY
+      if(V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4]) 
+        pc += 4;
+      else
+        pc +=2;
       break;
-    case 0x6000:
+    case 0x6000: //6XNN - Sets VX to NN
+      V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+      pc += 2;
       break;
-    case 0x7000:
+    case 0x7000: //// 0x7XNN - Adds NN to VX
+      V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
+      pc += 2;
       break;
     case 0x8000:
+      switch(opcode & 0x000F) {
+        case 0x0000: //8XY0 - Sets VX to the value of VY
+          V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
+          pc += 2;
+          break;
+        case 0x0001: //8XY1 - Sets VX to VX or VY
+          V[(opcode & 0x0F00) >> 8] |= V[(opcode & 0x00F0) >> 4];
+          pc += 2;
+          break;
+        case 0x0002: //8XY2 - Sets VX to VX and VY.
+          V[(opcode & 0x0F00) >> 8] &= V[(opcode & 0x00F0) >> 4];
+          pc += 2;
+          break;
+        case 0x0003: //8XY3 - Sets VX to VX xor VY.
+          V[(opcode & 0x0F00) >> 8] ^= V[(opcode & 0x00F0) >> 4];
+          pc += 2;
+          break;
+        case 0x0004: //8XY4 - Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't
+          V[(opcode & 0x0F00) >> 8] |= V[(opcode & 0x00F0) >> 4];
+          pc += 2;
+          break; 
+      }
       break;
     case 0x9000:
       break;
